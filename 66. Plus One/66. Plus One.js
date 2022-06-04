1072e2dc596d3b7d6828639ad3d479e7c3895054
JisanFahim <fahimjason@users.noreply.github.com>
2022-06-04 23:45:41 +0600

    trying-plus-one

--- 66. Plus One/66. Plus One.js
+++ 66. Plus One/66. Plus One.js
@@ -6,16 +6,27 @@ Link: https://leetcode.com/problems/plus-one/
 
 // Not solved yet
 
+// var plusOne = function (digits) {
+//     const end = digits.length - 1;
+//     if (digits[end] === 9 && digits.length === 1) {
+//         digits[end] = 1;
+//         digits.push(0);
+//     }
+//     else {
+//         digits[end] = digits[end] + 1;
+//     }
+//     return digits;
+// };
+
 var plusOne = function (digits) {
     const end = digits.length - 1;
-    if (digits[end] === 9 && digits.length === 1) {
-        digits[end] = 1;
-        digits.push(0);
+
+    let sum = '';
+    for (let i = 0; i < digits.length; i++) {
+        sum += digits[i];
     }
-    else {
-        digits[end] = digits[end] + 1;
-    }
-    return digits;
+
+    return sum;
 };
 
-//console.log(plusOne([9, 9]));
+console.log(plusOne([9, 9]));
