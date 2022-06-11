e0bbdb365cfa51e095acaa25c5c63cabf33aab6c
JisanFahim <fahimjason@users.noreply.github.com>
2022-06-11 16:36:07 +0600

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/125. Valid Palindrome.js
@@ -0,0 +1,17 @@
+var isPalindrome = function (s) {
+    // Sanitize the input sting
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+};
