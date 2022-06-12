9ee06a05c96747703f8fd4b237224cd065ab636c
JisanFahim <fahimjason@users.noreply.github.com>
2022-06-13 00:32:00 +0600

    242-Valid-Anagram-Resolved

---
+++ 242. Valid Anagram/242. Valid Anagram.js
@@ -0,0 +1,30 @@
+/*
+Problem: 242. Valid Anagram
+Link: https://leetcode.com/problems/valid-anagram/
+*/
+
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sCharCounts = {};  // {H: 1, E: 1, L: 2, O: 1}
+
+    // Fill sCharCounts
+    for (let i = 0; i < s.length; i++) {
+        const sChar = s[i];
+        sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1;
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        const tChar = t[i];
+
+        if (!sCharCounts[tChar]) {
+            return false;
+        } else {
+            sCharCounts[tChar]--;
+        }
+    }
+
+    return true;
+};
